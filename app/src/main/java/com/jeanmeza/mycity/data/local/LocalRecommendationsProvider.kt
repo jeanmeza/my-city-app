9924026d2b01215d4def5866ae3073009a00e9c2
Jean Meza <meza.jean@outlook.com>
2025-04-07 00:07:43 +0200

    add some recommendations to preview the screen

--- app/src/main/java/com/jeanmeza/mycity/data/local/LocalRecommendationsProvider.kt
+++ app/src/main/java/com/jeanmeza/mycity/data/local/LocalRecommendationsProvider.kt
@@ -1,11 +1,25 @@
 package com.jeanmeza.mycity.data.local
 
+import com.jeanmeza.mycity.R
 import com.jeanmeza.mycity.data.Recommendation
 
 object LocalRecommendationsProvider {
     fun getAllRecommendations(): List<Recommendation> {
         val categories = LocalCategoriesProvider.allCategories
         // TODO: return list of recommendations
-        return emptyList()
+        return listOf(
+            Recommendation(
+                id = 0,
+                category = categories[0],
+                imageResource = R.drawable.pasticceria_marescotti_cavo,
+                name = R.string.pasticceria_marescotti_cavo,
+            ),
+            Recommendation(
+                id = 0,
+                category = categories[0],
+                imageResource = R.drawable.caffe_degli_specchi,
+                name = R.string.caffe_degli_specchi,
+            ),
+        )
     }
 }
